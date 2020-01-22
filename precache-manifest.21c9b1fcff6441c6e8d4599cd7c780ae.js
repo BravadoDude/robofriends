dd4c2b34059f81f52fcf38d04f402ebb995aafbf
unknown <info@bravado.co.za>
2020-01-22 19:23:40 +0200

    Updates

---
+++ precache-manifest.21c9b1fcff6441c6e8d4599cd7c780ae.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55c2a98d9e462c3881238e2af17c759f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f44bee1ebf1ce400af5f",
+    "url": "/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "c63d810acb5892341236",
+    "url": "/static/css/main.33575798.chunk.css"
+  },
+  {
+    "revision": "f44bee1ebf1ce400af5f",
+    "url": "/static/js/2.84b5882a.chunk.js"
+  },
+  {
+    "revision": "83b3d04082a8c50f224f3e213e6a5a45",
+    "url": "/static/js/2.84b5882a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c63d810acb5892341236",
+    "url": "/static/js/main.ce23c358.chunk.js"
+  },
+  {
+    "revision": "e3f6cca2499cc0ccc243",
+    "url": "/static/js/runtime-main.b2109bab.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
